896356ff17016c6e41eee99539122fd085214e95
Mario Rodriguez <Mario_Rodriguez_36@progressive.com>
2024-06-05 14:00:25 -0400

    After lunch pull from Jeff

---
+++ logical-boundaries/help-desk/IssueTrackerSolution/HelpDeskSharedMessages/HelpDeskIssueCreated.cs
@@ -0,0 +1,14 @@
+﻿namespace HelpDeskSharedMessages;
+
+// What the IssueTracker API is going to send to the SupportApi
+public class HelpDeskIssueCreated
+{
+    public Guid Id { get; set; }
+    public Guid SoftwareId { get; set; }
+    public Guid UserId { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public string SoftwareTitle { get; set; } = string.Empty;
+    public string UserEmailAddress { get; set; } = string.Empty;
+
+
+}
